eee3d66ab97082412ccb9874f0d07e5a8b611b44
aurora2288 <82357098+aurora2288@users.noreply.github.com>
2021-04-12 12:12:17 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,49 @@
+let roll = 0
+input.onGesture(Gesture.Shake, function () {
+    roll = randint(1, 5)
+    if (roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (roll == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (roll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+    }
+    if (roll == 5) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+    }
+})
